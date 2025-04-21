e0f418027a81af322e31edb594c9029fee2d9286
KiShuKyu <krishnadhiman8527@gmail.com>
2025-04-21 23:39:09 +0530

    Scopes

---
+++ lesson_07/scopes.py
@@ -0,0 +1,27 @@
+x = 99  # global
+
+
+def f1():
+    x = 88
+
+    def f2():
+        print(x)
+    return f2
+
+
+myResult = f1()
+myResult()
+
+
+# Second (closur)
+def coder(num):
+    def actual(x):
+        return x ** num
+    return actual
+
+
+f = coder(2)
+g = coder(3)
+
+print(f(3))
+print(g(3))
