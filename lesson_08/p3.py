c98d33efedb999b0d658194926cf9af94e79ef73
KiShuKyu <krishnadhiman8527@gmail.com>
2025-04-22 12:01:09 +0530

    Problem 3

--- lesson_08/p3.py
+++ lesson_08/p3.py
@@ -1,1 +1,23 @@
 # Inheritance
+class Car:
+    # def __init__(self, elec, cng, petrol):
+    #     self.elec = elec
+    #     self.cng = cng
+    #     self.petrol = petrol
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        self.battery_size = battery_size
+        super().__init__(brand, model)  # upr hamne kr diya h
+
+    def full_name(self):
+        return f"{self.brand} {self.model} {self.battery_size}"
+
+
+my_car = ElectricCar("Tesla", "Rawr", "10m")
+print(my_car.model)
+print(my_car.full_name())
